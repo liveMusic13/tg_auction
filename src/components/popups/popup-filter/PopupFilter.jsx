0297380bc0a8d8886ecc1599449c4985969f42f2
Dmitriy <aegis.ko20@mail.ru>
2024-09-30 12:19:28 +0300

    add select checbox

--- src/components/popups/popup-filter/PopupFilter.jsx
+++ src/components/popups/popup-filter/PopupFilter.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import {
 	options_city,
 	options_color,
@@ -6,10 +8,177 @@ import {
 } from '../../../data/filterSettings.data';
 import ButtonIcon from '../../ui/button-icon/ButtonIcon';
 import Checkbox from '../../ui/checbox/Checkbox';
+import CheckboxCity from '../../ui/checbox/CheckboxCity';
 import FromTo from '../../ui/from-to/FromTo';
 import Range from '../../ui/range/Range';
 
 import styles from './PopupFilter.module.scss';
+
+// const PopupFilter = ({ setIsPopupFilter, button, objFunc }) => {
+// 	const {
+// 		setPrice,
+// 		setLong,
+// 		setNatural,
+// 		setColor,
+// 		setType,
+// 		setAge,
+// 		setWeight,
+// 		setCountry,
+// 		setCity,
+// 	} = objFunc;
+
+// 	const [isMoscowRegionOpen, setIsMoscowRegionOpen] = useState(false);
+// 	const [isBryanskRegionOpen, setIsBryanskRegionOpen] = useState(false);
+
+// 	const isLong = button === 'Длина';
+// 	const isPrice = button === 'Цена';
+// 	const isNatural_color = button === 'Натуральный цвет';
+// 	const isColor = button === 'Текущий цвет';
+// 	const isType = button === 'Тип';
+// 	const isAge = button === 'Возраст донора';
+// 	const isWeight = button === 'Вес';
+// 	const isCountry = button === 'Страна';
+// 	const isCity = button === 'Город';
+
+// 	const title = isLong
+// 		? 'Длина, см'
+// 		: isPrice
+// 			? 'Цена, ₽'
+// 			: isNatural_color
+// 				? 'Натуральный цвет'
+// 				: isColor
+// 					? 'Текущий цвет'
+// 					: isType
+// 						? 'Тип волос'
+// 						: isAge
+// 							? 'Возраст донора , лет'
+// 							: isWeight
+// 								? 'Вес , гр'
+// 								: button;
+
+// 	const option_checkbox =
+// 		isNatural_color || isColor
+// 			? options_color
+// 			: isType
+// 				? options_type
+// 				: isCountry
+// 					? options_country
+// 					: options_city;
+
+// 	const set_value = isLong ? setLong : isWeight ? setWeight : setAge;
+// 	const set_value_checkbox = isNatural_color
+// 		? setNatural
+// 		: isColor
+// 			? setColor
+// 			: isType
+// 				? setType
+// 				: isCountry
+// 					? setCountry
+// 					: setCity;
+
+// 	const options = [
+// 		{ option: 'Все' },
+// 		{ option: 'Московская область', subOptions: ['Москва', 'Подольск'] },
+// 		{
+// 			option: 'Брянская область',
+// 			subOptions: ['Брянск', 'Клинцы', 'Новозыбков'],
+// 		},
+// 	];
+
+// 	return (
+// 		<div className={styles.block__popup}>
+// 			<div className={styles.block__title}>
+// 				<h2 className={styles.title}>{title}</h2>
+// 				<ButtonIcon
+// 					data={{ src: '/images/icons/exit.svg' }}
+// 					onClick={() => setIsPopupFilter(false)}
+// 					style={{
+// 						backgroundColor: 'transparent',
+// 					}}
+// 					styleImg={{
+// 						width: 'calc(14/412*100vw)',
+// 						height: 'calc(14/412*100vw)',
+// 					}}
+// 				/>
+// 			</div>
+
+// 			{(isLong || isAge || isWeight) && (
+// 				<FromTo
+// 					data={
+// 						isAge
+// 							? {
+// 									min: 6,
+// 									max: 70,
+// 								}
+// 							: isWeight
+// 								? {
+// 										min: 10,
+// 										max: 1000,
+// 									}
+// 								: {
+// 										min: 10,
+// 										max: 120,
+// 									}
+// 					}
+// 					setValue={set_value}
+// 				/>
+// 			)}
+// 			{isPrice && <Range setValue={setPrice} />}
+// 			{(isNatural_color || isColor || isType || isCountry) && (
+// 				<Checkbox options={option_checkbox} setValue={set_value_checkbox} />
+// 			)}
+
+// 			{isCity && (
+// 				<>
+// 					<Checkbox
+// 						options={[option_checkbox[0]]}
+// 						setValue={set_value_checkbox}
+// 					/>
+
+// 					{/* Московская область
+// 					<div onClick={() => setIsMoscowRegionOpen(!isMoscowRegionOpen)}>
+// 						<h4>Московская область</h4>
+// 					</div>
+// 					{isMoscowRegionOpen && (
+// 						<Checkbox
+// 							options={[option_checkbox[1], option_checkbox[2]]}
+// 							setValue={set_value_checkbox}
+// 						/>
+// 					)} */}
+// 					{/* Московская область */}
+// 					<div onClick={() => setIsMoscowRegionOpen(!isMoscowRegionOpen)}>
+// 						<Checkbox
+// 							// options={['Московская область']}
+// 							options={options}
+// 							setValue={set_value_checkbox}
+// 						/>
+// 					</div>
+// 					{isMoscowRegionOpen && (
+// 						<Checkbox
+// 							options={[option_checkbox[1], option_checkbox[2]]}
+// 							setValue={set_value_checkbox}
+// 						/>
+// 					)}
+
+// 					{/* Брянская область */}
+// 					<div onClick={() => setIsBryanskRegionOpen(!isBryanskRegionOpen)}>
+// 						<h4>Брянская область</h4>
+// 					</div>
+// 					{isBryanskRegionOpen && (
+// 						<Checkbox
+// 							options={[
+// 								option_checkbox[3],
+// 								option_checkbox[4],
+// 								option_checkbox[5],
+// 							]}
+// 							setValue={set_value_checkbox}
+// 						/>
+// 					)}
+// 				</>
+// 			)}
+// 		</div>
+// 	);
+// };
 
 const PopupFilter = ({ setIsPopupFilter, button, objFunc }) => {
 	const {
@@ -23,6 +192,9 @@ const PopupFilter = ({ setIsPopupFilter, button, objFunc }) => {
 		setCountry,
 		setCity,
 	} = objFunc;
+
+	const [isMoscowRegionOpen, setIsMoscowRegionOpen] = useState(false);
+	const [isBryanskRegionOpen, setIsBryanskRegionOpen] = useState(false);
 
 	const isLong = button === 'Длина';
 	const isPrice = button === 'Цена';
@@ -70,6 +242,15 @@ const PopupFilter = ({ setIsPopupFilter, button, objFunc }) => {
 					? setCountry
 					: setCity;
 
+	const options = [
+		{ option: 'Все' },
+		{ option: 'Московская область', subOptions: ['Москва', 'Подольск'] },
+		{
+			option: 'Брянская область',
+			subOptions: ['Брянск', 'Клинцы', 'Новозыбков'],
+		},
+	];
+
 	return (
 		<div className={styles.block__popup}>
 			<div className={styles.block__title}>
@@ -109,8 +290,14 @@ const PopupFilter = ({ setIsPopupFilter, button, objFunc }) => {
 				/>
 			)}
 			{isPrice && <Range setValue={setPrice} />}
-			{(isNatural_color || isColor || isType || isCountry || isCity) && (
+			{(isNatural_color || isColor || isType || isCountry) && (
 				<Checkbox options={option_checkbox} setValue={set_value_checkbox} />
+			)}
+
+			{isCity && (
+				<>
+					<CheckboxCity options={options} setValue={set_value_checkbox} />
+				</>
 			)}
 		</div>
 	);
--- src/components/ui/checbox/Checkbox.jsx
+++ src/components/ui/checbox/Checkbox.jsx
@@ -6,19 +6,6 @@ const Checkbox = ({ options, setValue }) => {
 	// Стейт для отслеживания выбранных опций
 	const [selectedOptions, setSelectedOptions] = useState([]);
 
-	// // Функция для переключения состояния чекбокса
-	// const toggleOption = option => {
-	// 	if (selectedOptions.includes(option)) {
-	// 		// Убираем элемент из выбранных
-	// 		setSelectedOptions(selectedOptions.filter(item => item !== option));
-	// 		setValue(selectedOptions.filter(item => item !== option));
-	// 	} else {
-	// 		// Добавляем элемент в выбранные
-	// 		setSelectedOptions([...selectedOptions, option]);
-	// 		setValue([...selectedOptions, option]);
-	// 	}
-	// };
-
 	// Функция для переключения состояния чекбокса
 	const toggleOption = option => {
 		if (option === 'Все') {
@@ -77,5 +64,108 @@ const Checkbox = ({ options, setValue }) => {
 		</div>
 	);
 };
+
+// const Checkbox = ({ options, setValue }) => {
+// 	// Стейт для отслеживания выбранных опций
+// 	const [selectedOptions, setSelectedOptions] = useState([]);
+
+// 	// Функция для переключения состояния чекбокса
+// 	const toggleOption = (option, subOptions = []) => {
+// 		if (option === 'Все') {
+// 			// Если выбрано "Все", то отмечаем все чекбоксы
+// 			if (selectedOptions.includes('Все')) {
+// 				setSelectedOptions([]);
+// 				setValue([]);
+// 			} else {
+// 				const allOptions = options.flatMap(opt =>
+// 					opt.subOptions ? [opt.option, ...opt.subOptions] : opt.option,
+// 				);
+// 				setSelectedOptions(allOptions);
+// 				setValue(allOptions);
+// 			}
+// 		} else {
+// 			let newSelected;
+// 			// Если у опции есть подопции (города для региона)
+// 			if (subOptions.length > 0) {
+// 				if (selectedOptions.includes(option)) {
+// 					// Если регион уже выбран, то убираем его и его дочерние элементы
+// 					newSelected = selectedOptions.filter(
+// 						item => item !== option && !subOptions.includes(item),
+// 					);
+// 				} else {
+// 					// Если регион не выбран, добавляем его и его дочерние элементы
+// 					newSelected = [...selectedOptions, option, ...subOptions];
+// 				}
+// 			} else {
+// 				// Если это обычная опция без дочерних
+// 				if (selectedOptions.includes(option)) {
+// 					newSelected = selectedOptions.filter(item => item !== option);
+// 				} else {
+// 					newSelected = [...selectedOptions, option];
+// 				}
+// 			}
+// 			setSelectedOptions(newSelected);
+// 			setValue(newSelected);
+// 		}
+// 	};
+
+// 	// Автоматически отмечаем "Все", если выбраны все остальные опции
+// 	useEffect(() => {
+// 		const flatOptions = options.flatMap(opt =>
+// 			opt.subOptions ? [opt.option, ...opt.subOptions] : opt.option,
+// 		);
+// 		if (
+// 			selectedOptions.length === flatOptions.length - 1 &&
+// 			!selectedOptions.includes('Все')
+// 		) {
+// 			setSelectedOptions([...selectedOptions, 'Все']);
+// 			setValue([...selectedOptions, 'Все']);
+// 		}
+// 	}, [selectedOptions, options, setValue]);
+
+// 	return (
+// 		<div className={styles.checkboxContainer}>
+// 			{options.map(opt => (
+// 				<div key={opt.option} className={styles.checkboxItem}>
+// 					<div onClick={() => toggleOption(opt.option, opt.subOptions || [])}>
+// 						<span className={styles.label}>{opt.option}</span>
+// 						<img
+// 							src={
+// 								selectedOptions.includes(opt.option)
+// 									? '/images/icons/checkbox_active.svg'
+// 									: '/images/icons/checkbox.svg'
+// 							}
+// 							alt={opt.option}
+// 							className={styles.checkboxIcon}
+// 						/>
+// 					</div>
+// 					{/* Если есть дочерние опции, то отображаем их */}
+// 					{opt.subOptions && opt.subOptions.length > 0 && (
+// 						<div className={styles.subOptions}>
+// 							{opt.subOptions.map(subOption => (
+// 								<div
+// 									key={subOption}
+// 									className={styles.checkboxItem}
+// 									onClick={() => toggleOption(subOption)}
+// 								>
+// 									<span className={styles.label}>{subOption}</span>
+// 									<img
+// 										src={
+// 											selectedOptions.includes(subOption)
+// 												? '/images/icons/checkbox_active.svg'
+// 												: '/images/icons/checkbox.svg'
+// 										}
+// 										alt={subOption}
+// 										className={styles.checkboxIcon}
+// 									/>
+// 								</div>
+// 							))}
+// 						</div>
+// 					)}
+// 				</div>
+// 			))}
+// 		</div>
+// 	);
+// };
 
 export default Checkbox;
