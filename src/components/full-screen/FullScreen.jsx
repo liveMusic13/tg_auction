af5cbf20bf0a30c65cf9cdb628440e65fb11e213
Dmitriy <aegis.ko20@mail.ru>
2024-10-09 20:48:36 +0300

    test translate

--- src/components/full-screen/FullScreen.jsx
+++ src/components/full-screen/FullScreen.jsx
@@ -105,11 +105,16 @@ const FullScreen = ({ disableFullScreen, images }) => {
 	const [currentImageIndex, setCurrentImageIndex] = useState(0);
 	const [scale, setScale] = useState(1); // Начальное значение для масштаба
 	const [startDistance, setStartDistance] = useState(null); // Дистанция между пальцами в момент начала
+	const [translateX, setTranslateX] = useState(0); // Смещение по X
+	const [translateY, setTranslateY] = useState(0); // Смещение по Y
+	const [lastTouchX, setLastTouchX] = useState(0); // Последняя координата X
+	const [lastTouchY, setLastTouchY] = useState(0); // Последняя координата Y
 	const totalImages = images.length;
 	// Обработчик для двойного клика, увеличивает до половины максимального масштаба
 	const handleDoubleClick = () => {
-		console.log('Double click detected!'); // Для отладки
 		setScale(prevScale => (prevScale === 1.5 ? 1 : 1.5)); // Зум наполовину или сброс
+		setTranslateX(0);
+		setTranslateY(0); // Сброс положения при двойном клике
 	};
 	// Функция для смены изображения
 	const handleImageChange = direction => {
@@ -120,6 +125,9 @@ const FullScreen = ({ disableFullScreen, images }) => {
 				return prevIndex === 0 ? totalImages - 1 : prevIndex - 1;
 			}
 		});
+		setScale(1); // Сбрасываем масштаб при смене изображения
+		setTranslateX(0);
+		setTranslateY(0);
 	};
 
 	// Вычисление расстояния между двумя пальцами
@@ -130,15 +138,36 @@ const FullScreen = ({ disableFullScreen, images }) => {
 	};
 
 	const handleTouchStart = e => {
+		// if (e.touches.length === 2) {
+		// 	// Если есть два касания, запоминаем начальное расстояние между пальцами
+		// 	const distance = getDistance(e.touches[0], e.touches[1]);
+		// 	setStartDistance(distance);
+		// }
 		if (e.touches.length === 2) {
 			// Если есть два касания, запоминаем начальное расстояние между пальцами
 			const distance = getDistance(e.touches[0], e.touches[1]);
 			setStartDistance(distance);
+		} else if (e.touches.length === 1) {
+			// Запоминаем координаты при начале касания для одного пальца
+			setLastTouchX(e.touches[0].clientX);
+			setLastTouchY(e.touches[0].clientY);
 		}
 	};
 	const handleTouchMove = e => {
+		// if (e.touches.length === 2 && startDistance) {
+		// 	// Если есть два касания и мы начали отслеживание зума
+		// 	const newDistance = getDistance(e.touches[0], e.touches[1]);
+		// 	const scaleChange = newDistance / startDistance; // Соотношение новых и старых координат
+
+		// 	setScale(prevScale => {
+		// 		const newScale = prevScale * scaleChange; // Обновляем масштаб на основе разницы
+		// 		return Math.max(1, Math.min(newScale, 3)); // Ограничение масштаба от 1 до 3
+		// 	});
+
+		// 	setStartDistance(newDistance); // Обновляем дистанцию для следующего расчета
+		// }
 		if (e.touches.length === 2 && startDistance) {
-			// Если есть два касания и мы начали отслеживание зума
+			// Если два касания — это зум
 			const newDistance = getDistance(e.touches[0], e.touches[1]);
 			const scaleChange = newDistance / startDistance; // Соотношение новых и старых координат
 
@@ -146,8 +175,19 @@ const FullScreen = ({ disableFullScreen, images }) => {
 				const newScale = prevScale * scaleChange; // Обновляем масштаб на основе разницы
 				return Math.max(1, Math.min(newScale, 3)); // Ограничение масштаба от 1 до 3
 			});
-
 			setStartDistance(newDistance); // Обновляем дистанцию для следующего расчета
+		} else if (e.touches.length === 1 && scale > 1) {
+			// Если одно касание и масштаб больше 1 — это перемещение изображения
+			const touchX = e.touches[0].clientX;
+			const touchY = e.touches[0].clientY;
+
+			// Вычисляем смещение на основе движения пальца
+			setTranslateX(prevTranslateX => prevTranslateX + (touchX - lastTouchX));
+			setTranslateY(prevTranslateY => prevTranslateY + (touchY - lastTouchY));
+
+			// Обновляем последние координаты касания
+			setLastTouchX(touchX);
+			setLastTouchY(touchY);
 		}
 	};
 	const handleTouchEnd = () => {
@@ -198,7 +238,7 @@ const FullScreen = ({ disableFullScreen, images }) => {
 					src={images[currentImageIndex]}
 					alt={`Image ${currentImageIndex + 1}`}
 					style={{
-						transform: `scale(${scale})`, // Применяем масштабирование
+						transform: `scale(${scale}) scale(${scale}) translate(${translateX}px, ${translateY}px)`, // Применяем масштабирование
 						transition: 'transform 0.1s ease-in-out', // Плавная анимация изменения масштаба
 					}}
 					onTouchStart={handleTouchStart} // Начало касания
