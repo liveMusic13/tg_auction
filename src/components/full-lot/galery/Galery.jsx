686b87ee14b61e0c3f8c2cd6fa407a79cb3c37e9
Dmitriy <aegis.ko20@mail.ru>
2024-10-09 15:41:37 +0300

    add double zoom

--- src/components/full-lot/galery/Galery.jsx
+++ src/components/full-lot/galery/Galery.jsx
@@ -18,7 +18,7 @@ import styles from './Galery.module.scss';
 // 	} = usePlayer();
 // 	const videoRef = useRef();
 // 	const [currentImageIndex, setCurrentImageIndex] = useState(0);
-
+// 	const [scale, setScale] = useState(1); // Начальное значение для масштаба
 // 	const totalImages = lot.image.length;
 
 // 	// Функция для смены изображения
@@ -32,155 +32,13 @@ import styles from './Galery.module.scss';
 // 		});
 // 	};
 
-// 	// Обработчики свайпов
-// 	const handlers = useSwipeable({
-// 		onSwipedLeft: () => handleImageChange('next'),
-// 		onSwipedRight: () => handleImageChange('prev'),
-// 	});
-
-// 	return (
-// 		<div
-// 			className={isFullScreen ? styles.fullScreenSlider : styles.block__slider}
-// 			onClick={() => {
-// 				if (IS_PRO && !isFullScreen) {
-// 					activeFullScreen();
-// 				}
-// 			}}
-// 			{...handlers}
-// 		>
-// 			{isFullScreen && (
-// 				<button
-// 					className={styles.exit__fullScreen}
-// 					onClick={() => {
-// 						disableFullScreen();
-// 						setIsPlaying(false);
-// 					}}
-// 				>
-// 					<img src='/images/icons/buttons/close.svg' alt='Close' />
-// 				</button>
-// 			)}
-// 			{lot.image[currentImageIndex].includes('.mp4') ? (
-// 				<div className={styles.videoWrapper}>
-// 					{/* Видео с кастомной кнопкой воспроизведения */}
-// 					<video
-// 						className={styles.fullScreenImage}
-// 						ref={videoRef}
-// 						onClick={() => togglePlay(videoRef)}
-// 						onPlay={() => setIsPlaying(true)}
-// 						onPause={() => setIsPlaying(false)}
-// 						autoPlay={false}
-// 						poster={lot.image[0]} // Превью для видео
-// 					>
-// 						<source src={lot.image[currentImageIndex]} type='video/mp4' />
-// 						Your browser does not support the video tag.
-// 					</video>
-// 					{!isPlaying && IS_PRO && isFullScreen && (
-// 						<div
-// 							className={styles.playButtonOverlay}
-// 							onClick={() => togglePlay(videoRef)}
-// 						>
-// 							<img
-// 								src='/images/icons/buttons/play_video.svg'
-// 								alt='Play video'
-// 							/>
-// 						</div>
-// 					)}
-// 				</div>
-// 			) : (
-// 				// <img
-// 				// 	className={
-// 				// 		!isFullScreen ? styles.image__slider : styles.fullScreenImage
-// 				// 	}
-// 				// 	src={lot.image[currentImageIndex]}
-// 				// 	alt={`Image ${currentImageIndex + 1}`}
-// 				// />
-// 				// Добавляем компонент для зуммирования изображения
-// 				<PinchZoomPan
-// 					doubleTapZoom={2}
-// 					minScale={1}
-// 					maxScale={4}
-// 					className={
-// 						isFullScreen ? styles.fullScreenImage : styles.image__slider
-// 					}
-// 				>
-// 					<img
-// 						className={
-// 							isFullScreen ? styles.fullScreenImage : styles.image__slider
-// 						}
-// 						src={lot.image[currentImageIndex]}
-// 						alt={`Image ${currentImageIndex + 1}`}
-// 					/>
-// 				</PinchZoomPan>
-// 			)}
-
-// 			{isFullScreen && (
-// 				<div className={styles.thumbnailSlider}>
-// 					{lot.image.map((img, index) => (
-// 						<div
-// 							key={index}
-// 							className={`${styles.container__image} ${
-// 								currentImageIndex === index ? styles.activeThumbnail : ''
-// 							}`}
-// 							onClick={() => setCurrentImageIndex(index)}
-// 						>
-// 							<img
-// 								className={styles.thumbnailImage}
-// 								src={img.includes('.mp4') ? lot.image[0] : img}
-// 								alt={`Thumbnail ${index + 1}`}
-// 							/>
-// 							{!isPlaying && IS_PRO && isFullScreen && img.includes('.mp4') && (
-// 								<div
-// 									className={styles.playButtonOverlay}
-// 									style={{
-// 										width: 'calc(20/412*100vw)',
-// 										height: 'calc(20/412*100vw)',
-// 									}}
-// 								>
-// 									<img
-// 										src='/images/icons/buttons/play_video.svg'
-// 										alt='Play video'
-// 										style={{
-// 											width: 'calc(20/412*100vw)',
-// 											height: 'calc(20/412*100vw)',
-// 										}}
-// 									/>
-// 								</div>
-// 							)}
-// 						</div>
-// 					))}
-// 				</div>
-// 			)}
-// 			<p
-// 				className={styles.number__image}
-// 				style={isFullScreen ? { display: 'none' } : {}}
-// 			>
-// 				{currentImageIndex + 1}/{totalImages}
-// 			</p>
-// 		</div>
-// 	);
-// };
-
-// const Galery = ({ lot }) => {
-// 	const { isFullScreen } = useSelector(state => state.fullScreen);
-// 	const {
-// 		activeFullScreen,
-// 		disableFullScreen,
-// 		isPlaying,
-// 		setIsPlaying,
-// 		togglePlay,
-// 	} = usePlayer();
-// 	const videoRef = useRef();
-// 	const [currentImageIndex, setCurrentImageIndex] = useState(0);
-
-// 	const totalImages = lot.image.length;
-
-// 	// Функция для смены изображения
-// 	const handleImageChange = direction => {
-// 		setCurrentImageIndex(prevIndex => {
-// 			if (direction === 'next') {
-// 				return prevIndex === totalImages - 1 ? 0 : prevIndex + 1;
+// 	// Функция для увеличения/уменьшения масштаба
+// 	const handleZoom = direction => {
+// 		setScale(prevScale => {
+// 			if (direction === 'in') {
+// 				return Math.min(prevScale + 0.2, 3); // Максимальный масштаб 3
 // 			} else {
-// 				return prevIndex === 0 ? totalImages - 1 : prevIndex - 1;
+// 				return Math.max(prevScale - 0.2, 1); // Минимальный масштаб 1
 // 			}
 // 		});
 // 	};
@@ -189,10 +47,10 @@ import styles from './Galery.module.scss';
 // 	const handlers = useSwipeable({
 // 		onSwipedLeft: () => handleImageChange('next'),
 // 		onSwipedRight: () => handleImageChange('prev'),
+// 		onSwipedUp: () => handleZoom('in'), // Свайп вверх увеличивает
+// 		onSwipedDown: () => handleZoom('out'), // Свайп вниз уменьшает
 // 		delta: 100, // Увеличиваем минимальное расстояние для распознавания свайпа
 // 	});
-
-// 	const preventPan = () => true; // Всегда предотвращает панорамирование
 
 // 	return (
 // 		<div
@@ -243,172 +101,23 @@ import styles from './Galery.module.scss';
 // 					)}
 // 				</div>
 // 			) : isFullScreen ? (
-// 				<PanZoom
-// 					zoomSpeed={1.2}
-// 					minZoom={1}
-// 					maxZoom={3}
-// 					preventPan={preventPan}
-// 				>
-// 					<img
-// 						className={
-// 							isFullScreen ? styles.fullScreenImage : styles.image__slider
-// 						}
-// 						src={lot.image[currentImageIndex]}
-// 						alt={`Image ${currentImageIndex + 1}`}
-// 					/>
-// 				</PanZoom>
-// 			) : (
-// 				<img
-// 					className={
-// 						isFullScreen ? styles.fullScreenImage : styles.image__slider
-// 					}
-// 					src={lot.image[currentImageIndex]}
-// 					alt={`Image ${currentImageIndex + 1}`}
-// 				/>
-// 			)}
-
-// 			{isFullScreen && (
-// 				<div className={styles.thumbnailSlider}>
-// 					{lot.image.map((img, index) => (
-// 						<div
-// 							key={index}
-// 							className={`${styles.container__image} ${currentImageIndex === index ? styles.activeThumbnail : ''}`}
-// 							onClick={() => setCurrentImageIndex(index)}
-// 						>
-// 							<img
-// 								className={styles.thumbnailImage}
-// 								src={img.includes('.mp4') ? lot.image[0] : img}
-// 								alt={`Thumbnail ${index + 1}`}
-// 							/>
-// 							{!isPlaying && IS_PRO && isFullScreen && img.includes('.mp4') && (
-// 								<div
-// 									className={styles.playButtonOverlay}
-// 									style={{
-// 										width: 'calc(20/412*100vw)',
-// 										height: 'calc(20/412*100vw)',
-// 									}}
-// 								>
-// 									<img
-// 										src='/images/icons/buttons/play_video.svg'
-// 										alt='Play video'
-// 										style={{
-// 											width: 'calc(20/412*100vw)',
-// 											height: 'calc(20/412*100vw)',
-// 										}}
-// 									/>
-// 								</div>
-// 							)}
-// 						</div>
-// 					))}
-// 				</div>
-// 			)}
-// 			<p
-// 				className={styles.number__image}
-// 				style={isFullScreen ? { display: 'none' } : {}}
-// 			>
-// 				{currentImageIndex + 1}/{totalImages}
-// 			</p>
-// 		</div>
-// 	);
-// };
-
-// const Galery = ({ lot }) => {
-// 	const { isFullScreen } = useSelector(state => state.fullScreen);
-// 	const {
-// 		activeFullScreen,
-// 		disableFullScreen,
-// 		isPlaying,
-// 		setIsPlaying,
-// 		togglePlay,
-// 	} = usePlayer();
-// 	const videoRef = useRef();
-// 	const [currentImageIndex, setCurrentImageIndex] = useState(0);
-
-// 	const totalImages = lot.image.length;
-
-// 	// Функция для смены изображения
-// 	const handleImageChange = direction => {
-// 		setCurrentImageIndex(prevIndex => {
-// 			if (direction === 'next') {
-// 				return prevIndex === totalImages - 1 ? 0 : prevIndex + 1;
-// 			} else {
-// 				return prevIndex === 0 ? totalImages - 1 : prevIndex - 1;
-// 			}
-// 		});
-// 	};
-
-// 	// Обработчики свайпов
-// 	const handlers = useSwipeable({
-// 		onSwipedLeft: () => handleImageChange('next'),
-// 		onSwipedRight: () => handleImageChange('prev'),
-// 		delta: 100, // Увеличиваем минимальное расстояние для распознавания свайпа
-// 	});
-
-// 	const preventPan = () => true; // Всегда предотвращает панорамирование
-
-// 	return (
-// 		<div
-// 			className={isFullScreen ? styles.fullScreenSlider : styles.block__slider}
-// 			onClick={() => {
-// 				if (IS_PRO && !isFullScreen) {
-// 					activeFullScreen();
-// 				}
-// 			}}
-// 			{...handlers}
-// 		>
-// 			{isFullScreen && (
-// 				<button
-// 					className={styles.exit__fullScreen}
-// 					onClick={() => {
-// 						disableFullScreen();
-// 						setIsPlaying(false);
+// 				<div
+// 					style={{
+// 						overflow: 'hidden',
+// 						transform: `scale(${scale})`, // Применяем масштабирование
+// 						transition: 'transform 0.3s ease-in-out', // Плавная анимация изменения масштаба
 // 					}}
 // 				>
-// 					<img src='/images/icons/buttons/close.svg' alt='Close' />
-// 				</button>
-// 			)}
-// 			{lot.image[currentImageIndex].includes('.mp4') ? (
-// 				<div className={styles.videoWrapper}>
-// 					{/* Видео с кастомной кнопкой воспроизведения */}
-// 					<video
-// 						className={styles.fullScreenImage}
-// 						ref={videoRef}
-// 						onClick={() => togglePlay(videoRef)}
-// 						onPlay={() => setIsPlaying(true)}
-// 						onPause={() => setIsPlaying(false)}
-// 						autoPlay={false}
-// 						poster={lot.image[0]} // Превью для видео
-// 					>
-// 						<source src={lot.image[currentImageIndex]} type='video/mp4' />
-// 						Your browser does not support the video tag.
-// 					</video>
-// 					{!isPlaying && IS_PRO && isFullScreen && (
-// 						<div
-// 							className={styles.playButtonOverlay}
-// 							onClick={() => togglePlay(videoRef)}
-// 						>
-// 							<img
-// 								src='/images/icons/buttons/play_video.svg'
-// 								alt='Play video'
-// 							/>
-// 						</div>
-// 					)}
-// 				</div>
-// 			) : isFullScreen ? (
-// 				<PanZoom
-// 					zoomSpeed={1.2}
-// 					minZoom={1}
-// 					maxZoom={3}
-// 					preventPan={preventPan}
-// 				>
 // 					<img
-// 						className={
-// 							isFullScreen ? styles.fullScreenImage : styles.image__slider
-// 						}
+// 						className={styles.fullScreenImage}
 // 						src={lot.image[currentImageIndex]}
 // 						alt={`Image ${currentImageIndex + 1}`}
+// 						style={{
+// 							transform: `scale(${scale})`, // Применяем масштабирование
+// 							transition: 'transform 0.3s ease-in-out', // Плавная анимация изменения масштаба
+// 						}}
 // 					/>
-// 				</PanZoom>
+// 				</div>
 // 			) : (
 // 				<img
 // 					className={
@@ -432,24 +141,6 @@ import styles from './Galery.module.scss';
 // 								src={img.includes('.mp4') ? lot.image[0] : img}
 // 								alt={`Thumbnail ${index + 1}`}
 // 							/>
-// 							{!isPlaying && IS_PRO && isFullScreen && img.includes('.mp4') && (
-// 								<div
-// 									className={styles.playButtonOverlay}
-// 									style={{
-// 										width: 'calc(20/412*100vw)',
-// 										height: 'calc(20/412*100vw)',
-// 									}}
-// 								>
-// 									<img
-// 										src='/images/icons/buttons/play_video.svg'
-// 										alt='Play video'
-// 										style={{
-// 											width: 'calc(20/412*100vw)',
-// 											height: 'calc(20/412*100vw)',
-// 										}}
-// 									/>
-// 								</div>
-// 							)}
 // 						</div>
 // 					))}
 // 				</div>
@@ -489,24 +180,31 @@ const Galery = ({ lot }) => {
 		});
 	};
 
-	// Функция для увеличения/уменьшения масштаба
-	const handleZoom = direction => {
+	// Функция для изменения масштаба в реальном времени
+	const handleZoomDuringSwipe = deltaY => {
 		setScale(prevScale => {
-			if (direction === 'in') {
-				return Math.min(prevScale + 0.2, 3); // Максимальный масштаб 3
-			} else {
-				return Math.max(prevScale - 0.2, 1); // Минимальный масштаб 1
-			}
+			const newScale = prevScale - deltaY / 500; // Регулируем масштаб на основе свайпа
+			return Math.max(1, Math.min(newScale, 3)); // Ограничение масштаба от 1 до 3
 		});
 	};
 
-	// Обработчики свайпов
+	// Обработчик для двойного клика, увеличивает до половины максимального масштаба
+	const handleDoubleClick = () => {
+		console.log('Double click detected!'); // Для отладки
+		setScale(prevScale => (prevScale === 1.5 ? 1 : 1.5)); // Зум наполовину или сброс
+	};
+
+	// Обработчики свайпов с зумом в реальном времени
 	const handlers = useSwipeable({
 		onSwipedLeft: () => handleImageChange('next'),
 		onSwipedRight: () => handleImageChange('prev'),
-		onSwipedUp: () => handleZoom('in'), // Свайп вверх увеличивает
-		onSwipedDown: () => handleZoom('out'), // Свайп вниз уменьшает
-		delta: 100, // Увеличиваем минимальное расстояние для распознавания свайпа
+		onSwiping: eventData => {
+			// Определяем, если свайп вверх/вниз и изменяем масштаб
+			if (eventData.dir === 'Up' || eventData.dir === 'Down') {
+				handleZoomDuringSwipe(eventData.deltaY);
+			}
+		},
+		delta: 10, // Чувствительность к свайпу
 	});
 
 	return (
@@ -558,21 +256,16 @@ const Galery = ({ lot }) => {
 					)}
 				</div>
 			) : isFullScreen ? (
-				<div
-					style={{ overflow: 'hidden' }}
-					style={{
-						transform: `scale(${scale})`, // Применяем масштабирование
-						transition: 'transform 0.3s ease-in-out', // Плавная анимация изменения масштаба
-					}}
-				>
+				<div style={{ overflow: 'hidden' }}>
 					<img
 						className={styles.fullScreenImage}
 						src={lot.image[currentImageIndex]}
 						alt={`Image ${currentImageIndex + 1}`}
 						style={{
 							transform: `scale(${scale})`, // Применяем масштабирование
-							transition: 'transform 0.3s ease-in-out', // Плавная анимация изменения масштаба
+							transition: 'transform 0.1s ease-in-out', // Плавная анимация изменения масштаба
 						}}
+						onDoubleClick={handleDoubleClick} // Обрабатываем двойной клик
 					/>
 				</div>
 			) : (
